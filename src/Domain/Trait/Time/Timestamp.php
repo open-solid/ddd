d7f0e5d4c3b31ad825463dd56a3a746097af40d9
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-10-20 16:02:32 -0400

    add Timestamp traits

---
+++ src/Domain/Trait/Time/Timestamp.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Ddd\Domain\Trait\Time;
+
+use DateTimeImmutable;
+
+trait Timestamp
+{
+    private readonly DateTimeImmutable $createdAt;
+    private ?DateTimeImmutable $updatedAt = null;
+
+    public function createdAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function updatedAt(): ?DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+}
---
+++ src/Domain/Trait/Time/TimestampCreatedAt.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ddd\Domain\Trait\Time;
+
+use DateTimeImmutable;
+
+trait TimestampCreatedAt
+{
+    private readonly DateTimeImmutable $createdAt;
+
+    public function createdAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+}
